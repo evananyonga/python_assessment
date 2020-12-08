c3cc5bb3611c27e83f56100a975d11691a7721fa
TheInvincible <evewish@gmail.com>
2020-12-08 11:23:57 +0300

    update to name_and_age

--- name_and_age.py
+++ name_and_age.py
@@ -1,4 +1,14 @@
 # A programme that prints name and age
 
-first_name, last_name, age = input("Enter your First Name, Last Name, Age: ").split()
-print(first_name, last_name, age)
+all_ages = []
+for user in range(0, 3):
+    first_name, last_name, age = input("Enter your First Name, Last Name, Age: ").split()
+    numbers = int(age)
+    # full_friends.extend([first_name, last_name, age])
+    print(first_name, last_name, numbers)
+
+    # for age in range(numbers):
+    all_ages.append(numbers)
+
+print('Total of age is : {}'.format(sum(all_ages)))
+print('Average Age is : {}'.format(sum(all_ages)/len(all_ages)))
